ed3181bdaf0cbce79d157a2973b526988d23ed20
arthas <arthas@debian.arthas>
2025-04-16 23:18:04 +0500

    Added root route to handle 404 error

--- backend/main.py
+++ backend/main.py
@@ -48,3 +48,6 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     # Запускаем приложение на всех адресах, чтобы оно было доступно извне
     app.run(host="0.0.0.0", port=port)
+@app.route("/", methods=["GET"])
+def home():
+    return "Hello, welcome to Urassist!"  # Или возвращайте HTML страницу, если необходимо
